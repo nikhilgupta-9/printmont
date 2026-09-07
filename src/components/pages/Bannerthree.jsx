c9fd701e4e43e499cde6f9bcd3819c0eecaa927b
nikhilgupta-9 <iamnikhilgupta9@gmail.com>
2026-09-07 17:59:03 +0530

    Deploy latest frontend build

--- src/components/pages/Bannerthree.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import StaticResponsiveBanner from "../home/banners/StaticResponsiveBanner";
-
-const Bannerthree = (props) => {
-  return <StaticResponsiveBanner {...props} />;
-};
-
-export default Bannerthree;
--- src/components/pages/TermsOfUse.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PolicyPage from '../policy/PolicyPage';
-
-const TermsOfUse = () => {
-  return <PolicyPage />;
-};
-
-export default TermsOfUse;
